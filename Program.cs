5611d5a49be18bac979d7f0055d9739f49fb4a28
Greyser0 <serjcool2@gmail.com>
2024-10-28 19:10:38 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Найти сумму двух целых (вещественных) чисел и вывести результат на экран.
+            //Console.Write("Первое число: ");
+            //int number_1 = int.Parse(Console.ReadLine());
+            //Console.Write("Второе число: ");
+            //int number_2 = int.Parse(Console.ReadLine());
+            //int sum = number_1 + number_2;
+            //Console.WriteLine($"Сумма: {sum}");
+            //Console.Read();
+
+            //Вычислить периметр квадрата и вывести результат на экран.
+            //Console.Write("Сторона: ");
+            //int side = int.Parse(Console.ReadLine());
+            //int perimeter = side * 4;
+            //Console.WriteLine($"Периметр = {perimeter}");
+            //Console.Read();
+
+            //Вычислить длину окружности и площадь круга и вывести результаты на экран.
+            //Console.Write("Радиус круга: ");
+            //int rad = int.Parse(Console.ReadLine());
+            //double area = Math.PI * Math.Pow(rad, 2);
+            //double length = Math.PI * 2 * rad;
+            //Console.WriteLine($"Площадь = {area:f2}");
+            //Console.WriteLine($"Длина = {length:f2}");
+            //Console.Read();
+        }
+    }
+}
